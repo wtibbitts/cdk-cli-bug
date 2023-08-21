725d88f763b7e4594f2eba2984426df1296ad348
Wes Tibbitts <wes@eturi.com>
2023-08-21 09:28:48 -0700

    simple project

--- lib/cdk-cli-diff-bug-stack.ts
+++ lib/cdk-cli-diff-bug-stack.ts
@@ -1,16 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkCliDiffBugStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { User } from 'aws-cdk-lib/aws-iam'
+import {
+  Cluster,
+  KubernetesVersion,
+} from 'aws-cdk-lib/aws-eks';
+import { KubectlV26Layer } from '@aws-cdk/lambda-layer-kubectl-v26';
+import { STSClient, GetCallerIdentityCommand, GetCallerIdentityCommandOutput } from '@aws-sdk/client-sts'
+import { Construct } from 'constructs';
+import * as util from 'util';
+
+
+async function getCallerIdentity(): Promise<GetCallerIdentityCommandOutput> {
+  const client = new STSClient({ region: process.env.CDK_DEFAULT_REGION });
+  const command = new GetCallerIdentityCommand({});
+  return client.send(command)
+}
+
+export class CdkCliDiffBugStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cluster = new Cluster(this, 'Cluster', {
+      clusterName: 'dummyCluster',
+      defaultCapacity: 0, 
+      kubectlLayer: new KubectlV26Layer(this, 'KubeClientUtils'),
+      outputClusterName: true,
+      version: KubernetesVersion.of('1.27'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCliDiffBugQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    getCallerIdentity().then((user) => {
+      console.log(`current user: ${util.inspect(user, { depth: null, colors: true })}`)
+      if (user?.Arn) { 
+        const iamuser = User.fromUserArn(this, `Admin${user.UserId}`, user.Arn)
+        console.log(`iamuser: ${iamuser.userName}, ${iamuser.userArn}`);
+        cluster.awsAuth.addUserMapping(iamuser, { groups: ['system:masters'] })
+      } else {
+        console.error('Unable to get current user');
+      }
+    });
   }
 }
